3e7c73c56dc66cf5de382a91b03ac83e174d621c
phanindra <phaninan153@gmail.com>
2020-08-04 12:45:38 +0530

    Added Loading screen

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import Verification from './src/screens/Verification'
 import Login from './src/screens/Login'
 import Signup from './src/screens/Signup'
 import RegisterSuccess from './src/screens/RegisterSuccess'
+import Loading from './src/screens/Loading'
 
 
 const Stack = createStackNavigator()
@@ -16,7 +17,8 @@ function App() {
   return (
     <NavigationContainer>
       <StatusBar barStyle="light-content" backgroundColor="#002F72" />
-      <Stack.Navigator initialRouteName={MobileNumber}>
+      <Stack.Navigator initialRouteName={Loading}>
+        <Stack.Screen name="Loading" component={Loading} options={{ headerShown: false }} />
         <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
         <Stack.Screen name="MobileNumber" component={MobileNumber} options={{ headerShown: false }} />
         <Stack.Screen name="Verification" component={Verification} options={{ headerShown: false }} />
---
+++ src/screens/Loading.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+import { View, Text, Image } from 'react-native'
+import Login from './Login'
+
+function Loading({ navigation }) {
+
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        const callScreen = () => {
+            setTimeout(() => {
+                setIsLoading(false)
+                navigation.navigate('Login')
+            }, 2000)
+        }
+        callScreen()
+        // navigation.navigate('Login', setIsLoading(false))
+
+    }, [])
+
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#fff' }}>
+            {
+                isLoading ?
+                    <Image source={require('../../assets/Logo.jpeg')} style={{ width: 100, height: 100, borderRadius: 100, borderWidth: 2, borderColor: '#002F72', }} />
+                    : null
+            }
+        </View>
+    )
+}
+
+export default Loading
